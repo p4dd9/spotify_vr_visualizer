15e7103c6bb3cf519bfcd4f8514c73c2ea861add
Patrick Obermüller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-05-21 16:12:51 +0200

    Add config for webpack dev server and directories.js

---
+++ config/directories.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const PATHS = Object.freeze({
+  src: path.resolve(__dirname, '../app/src'),
+  dist: path.resolve(__dirname, '../app/dist'),
+});
+
+export default PATHS;
---
+++ config/serverOptions.js
@@ -0,0 +1,7 @@
+const OPTIONS = Object.freeze({
+  host: 'localhost',
+  port: 3000,
+  server: {baseDir: ['app/dist']}
+});
+
+export default OPTIONS;
